08925a256038e0d5d1ff217b38f2498f39f282ff
fturannn <furkn.turan@hotmail.com>
2023-09-12 10:08:59 +0300

    Daire diliminin alanını hesaplayan program oluşturuldu

--- Week-1/DaireDilim/src/DaireDilim.java
+++ Week-1/DaireDilim/src/DaireDilim.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class DaireDilim {
+import java.util.Scanner;
+public class DaireDilim {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double yaricap, aci, daireDilimAlan;
+
+        // Kullanıcıdan yarıçap ve açı ölçüsünü alalım
+        System.out.print("Dairenin yarıçapını giriniz: ");
+        yaricap = input.nextDouble();
+
+        System.out.print("Alanını ölçmek istediğiniz daire diliminin açısını giriniz: ");
+        aci = input.nextDouble();
+
+        // Açısı ve yarıçapı verilen daire diliminin alanını hesaplayalım ve ekrana yazdıralım
+        daireDilimAlan = (3.14 * yaricap * yaricap * aci) / 360;
+        System.out.println("Açısı ve yarıçapı verilen daire diliminin alanı: " + daireDilimAlan);
+    }
 }
