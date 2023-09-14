6a3f616ebcf706c12b7d314afcc3f6f4d3973a56
fturannn <furkn.turan@hotmail.com>
2023-09-14 14:02:00 +0300

    Min ve Max değerleri bulan program eklendi

---
+++ Week-1/MinMax/src/MinMax.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class MinMax {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int adet, sayi, max = 0, min = 0;
+
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        adet = input.nextInt();
+
+        System.out.print("1. sayıyı giriniz: "); // İlk girilen sayıyı min ve max değişkenlerine atayalım
+        min = input.nextInt();
+        max = min;
+
+        for (int i = 2; i <= adet; i++) {
+            System.out.print(i + ". sayıyı giriniz: ");
+            sayi = input.nextInt();
+            if (sayi >= max) {
+                max = sayi;
+            } else if (sayi <= min) {
+                min = sayi;
+            }
+        }
+
+        // En büyük ve en küçük sayıları ekrana yazdıralım
+        System.out.println("En büyük sayı: " + max);
+        System.out.print("En küçük sayı: " + min);
+    }
+}
