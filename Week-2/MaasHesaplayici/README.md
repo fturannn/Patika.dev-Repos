96c96c94350c2fe162d95fcf364d3918a171b80f
fturannn <furkn.turan@hotmail.com>
2023-09-20 12:08:09 +0300

    Maaş Hesaplayıcı Programı eklenmiştir

---
+++ Week-2/MaasHesaplayici/README.md
@@ -0,0 +1,10 @@
+# Maaş Hesaplayıcı
+
+Java'da "Employee" adında fabrika çalışanlarını temsil eden ve metotları ile çalışanların maaşlarını hesaplayan bir sınıf içeren programdır.
+
+- Çalışanın maaşı 1000 TL'den az ise vergi uygulanmayacaktır.
+- Çalışanın maaşı 1000 TL'den fazla ise maaşının %3'ü kadar vergi uygulanacaktır.
+- Eğer çalışan haftada 40 saatten fazla çalışmış ise fazladan çalıştığı her saat başına 30 TL olacak şekilde bonus ücret alacaktır.
+- Eğer çalışan 10 yıldan az bir süredir çalışıyorsa maaşına %5 zam yapılacaktır.
+- Eğer çalışan 9 yıldan fazla ve 20 yıldan az çalışıyorsa maaşına %10 zam yapılacaktır.
+- ğer çalışan 19 yıldan fazla çalışıyorsa %15 zam yapılacaktır.
---
+++ Week-2/MaasHesaplayici/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name;
+        int salary, workHours, hireYear;
+
+        System.out.print("İşçinin ismini giriniz: ");
+        name = input.nextLine();
+
+        System.out.print("İşçinin maaşını giriniz: ");
+        salary = input.nextInt();
+
+        System.out.print("İşçinin çalışma saatini giriniz: ");
+        workHours = input.nextInt();
+
+        System.out.print("İşçinin işe giriş tarihini giriniz: ");
+        hireYear = input.nextInt();
+
+        Employee e1 = new Employee(name, salary, workHours, hireYear);
+        e1.toStrings();
+
+    }
+}
