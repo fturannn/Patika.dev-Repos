c337074b5ed7f14b17d1cedfe70ce7e388ad3a97
fturannn <furkn.turan@hotmail.com>
2023-10-10 20:25:23 +0300

    Programda düzeltme yapıldı

--- Week-5/KitapSiralayici/src/Main.java
+++ Week-5/KitapSiralayici/src/Main.java
@@ -10,11 +10,7 @@ public class Main {
         book.add(new Book("Şeker Portakalı", 182, "José Mauro de Vasconcelos", 1968));
         book.add(new Book("Dönüşüm", 104, "Franz Kafka", 1915));
 
-        book2.add(new Book("Hayvan Çiftliği", 152, "George Orwell", 1945));
-        book2.add(new Book("Satranç", 192, "Stefan Zweig", 1942));
-        book2.add(new Book("Küçük Prens", 112, "Antoine de Saint-Exupéry", 1945));
-        book2.add(new Book("Şeker Portakalı", 182, "José Mauro de Vasconcelos", 1968));
-        book2.add(new Book("Dönüşüm", 104, "Franz Kafka", 1915));
+        book2.addAll(book);
 
         for(Book b : book) {
             System.out.println(b.getName() + " " + b.getPage()); // Sayfa adına göre sıralama
