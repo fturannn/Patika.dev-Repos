087dd74d8e89f76c4f2386ee34478822a695ae2a
Furkan Turan <106929726+fturannn@users.noreply.github.com>
2023-11-14 00:45:40 +0300

    Delete Week-8/TurizmAcenteSistemi directory

--- Week-8/TurizmAcenteSistemi/src/com/turizmacenta/Helper/Config.java
+++
@@ -1,8 +0,0 @@
-package com.turizmacenta.Helper;
-
-public class Config {
-    public static final String PROJECT_TITLE = "Turizm Acenta Sistemi";
-    public static final String DB_URL = "jdbc:mysql://localhost/turizm_acenta";
-    public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "mysql";
-}
