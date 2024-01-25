19e699de514fdc04d6e271c00f6a67220c11355c
fturannn <furkn.turan@hotmail.com>
2024-01-25 22:08:34 +0300

    To Do List ödevi yüklendi

--- Week-20/todolist/script.js
+++ Week-20/todolist/script.js
@@ -1,19 +1,6 @@
 'use strict'
 
 let ul = document.getElementById("list");
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-
-/*
-var myNodelist = document.getElementsByTagName("LI"); // X butonu ekleme
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-*/
 
 var close = document.getElementsByClassName("close"); // Liste elemanını silme
 var i;
@@ -23,28 +10,6 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   };
 }
-
-/*
-for(let i = 0; itemsArray.length; i++) {
-    let li = document.createElement("li");
-    li.textContent = itemsArray[i];
-    ul.appendChild(li);
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    if(close.length > 0) {
-        for (i = 0; i < close.length; i++) {
-            close[i].onclick = function () {
-                var div = this.parentElement;
-                div.style.display = "none";
-            };
-        }
-    }
-}
-*/
 
 function newElement() {
     let li = document.createElement("li");
@@ -65,8 +30,6 @@ function newElement() {
         var toastList = toastElList.map(function(toastEl) {
         return new bootstrap.Toast(toastEl)
         })
-        // itemsArray.push(inputValue);
-        // localStorage.setItem("items", JSON.stringify(itemsArray));
         toastList.forEach(toast => toast.show())
         text.value = '';
 
