fd3da95056b274631c61af48eb974f31a141e30a
fturannn <furkn.turan@hotmail.com>
2024-02-13 11:14:26 +0300

    PageContent bölümündeki bug düzeltildi

--- Week-22/bill-gates/src/components/PageContent.jsx
+++ Week-22/bill-gates/src/components/PageContent.jsx
@@ -11,29 +11,31 @@ const PageContent = () => {
   return (
     <div className="page-content">
       {data.map((item) => {
+        const [count, setCount] = useState(0);
 
         function buy() {
+          setCount(count + 1);
           item.count += 1;
           setTotalWorth(totalWorth - item.price)
         }
 
         function sell() {
-          if (item.count > 0) {
+          if (count > 0) {
+            setCount(count - 1);
             item.count -= 1;
             setTotalWorth(totalWorth + item.price)
           }
         }
-
         return (
           <div key={item.title} className="item">
             <img src={item.image} alt={item.title} />
             <h3>{item.title}</h3>
             <p>${item.price}</p>
             <div className="buttons">
-              <button className="button sell-button" onClick={sell}>
+              <button className="button" onClick={sell}>
                 Sell
               </button>
-              <input type="text" className="count" value={item.count} />
+              <input type="text" className="count" value={count} />
               <button className="buy button" onClick={buy}>
                 Buy
               </button>
